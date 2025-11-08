26eea0f6a94e435e13280ea14a3b73158da48c22
Chiwaki <chiwa2023@ymail.ne.jp>
2025-11-08 20:42:27 +0900

    add front server設定

--- app/front/vite.config.ts
+++ app/front/vite.config.ts
@@ -4,5 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/manage-kanrensha'
+  base: '/manage-kanrensha',
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['host.docker.internal']
+  },
+
 })
